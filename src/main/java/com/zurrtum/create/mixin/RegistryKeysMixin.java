b99d6e31357749e3d31dd6ac3078191cb5081a8f
ZurrTum <zurrtum@foxmail.com>
2025-09-20 01:56:06 +0800

    Fix Fabric API load potato projectile

---
+++ src/main/java/com/zurrtum/create/mixin/RegistryKeysMixin.java
@@ -0,0 +1,23 @@
+package com.zurrtum.create.mixin;
+
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import static com.zurrtum.create.Create.MOD_ID;
+
+@Mixin(RegistryKeys.class)
+public class RegistryKeysMixin {
+    @Inject(method = "getPath(Lnet/minecraft/registry/RegistryKey;)Ljava/lang/String;", at = @At("HEAD"), cancellable = true)
+    private static void getPath(RegistryKey<? extends Registry<?>> registryRef, CallbackInfoReturnable<String> cir) {
+        Identifier id = registryRef.getValue();
+        if (id.getNamespace().equals(MOD_ID)) {
+            cir.setReturnValue(id.getPath());
+        }
+    }
+}
