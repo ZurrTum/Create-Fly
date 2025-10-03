52885200e75f4386dd4025f73dd1db8bf2518491
ZurrTum <zurrtum@foxmail.com>
2025-10-04 02:59:03 +0800

    Fix multiplayer mode wand of symmetry

--- src/client/java/com/zurrtum/create/client/mixin/ItemStackMixin.java
+++ src/client/java/com/zurrtum/create/client/mixin/ItemStackMixin.java
@@ -2,26 +2,16 @@ package com.zurrtum.create.client.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.sugar.Local;
-import com.llamalad7.mixinextras.sugar.Share;
-import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import com.zurrtum.create.client.foundation.item.TooltipModifier;
 import com.zurrtum.create.client.infrastructure.config.AllConfigs;
 import com.zurrtum.create.client.ponder.foundation.PonderTooltipHandler;
-import com.zurrtum.create.content.contraptions.glue.SuperGlueHandler;
-import com.zurrtum.create.content.equipment.symmetryWand.SymmetryHandler;
-import net.minecraft.component.DataComponentTypes;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.*;
-import net.minecraft.server.world.ServerWorld;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 
@@ -40,38 +30,5 @@ public abstract class ItemStackMixin {
             modifier.modify(tooltip, player);
         }
         return tooltip;
-    }
-
-    @Inject(method = "useOnBlock(Lnet/minecraft/item/ItemUsageContext;)Lnet/minecraft/util/ActionResult;", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/Item;useOnBlock(Lnet/minecraft/item/ItemUsageContext;)Lnet/minecraft/util/ActionResult;"))
-    private void cacheState(
-        ItemUsageContext context,
-        CallbackInfoReturnable<ActionResult> cir,
-        @Local Item item,
-        @Share("place") LocalRef<ItemPlacementContext> place
-    ) {
-        if (item instanceof BlockItem) {
-            World world = context.getWorld();
-            if (!world.isClient) {
-                place.set(new ItemPlacementContext(context));
-            }
-        }
-    }
-
-    @Inject(method = "useOnBlock(Lnet/minecraft/item/ItemUsageContext;)Lnet/minecraft/util/ActionResult;", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/ActionResult$Success;shouldIncrementStat()Z"))
-    private void useOnBlock(
-        ItemUsageContext context,
-        CallbackInfoReturnable<ActionResult> cir,
-        @Local PlayerEntity player,
-        @Share("place") LocalRef<ItemPlacementContext> place
-    ) {
-        ItemPlacementContext placementContext = place.get();
-        if (placementContext != null) {
-            ServerWorld world = (ServerWorld) context.getWorld();
-            BlockPos pos = placementContext.getBlockPos();
-            SuperGlueHandler.glueListensForBlockPlacement(world, player, pos);
-            if (!context.getStack().components.contains(DataComponentTypes.CONSUMABLE)) {
-                SymmetryHandler.onBlockPlaced(world, player, pos, placementContext);
-            }
-        }
     }
 }
---
+++ src/main/java/com/zurrtum/create/mixin/ItemStackMixin.java
@@ -0,0 +1,54 @@
+package com.zurrtum.create.mixin;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
+import com.zurrtum.create.content.contraptions.glue.SuperGlueHandler;
+import com.zurrtum.create.content.equipment.symmetryWand.SymmetryHandler;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.*;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ItemStack.class)
+public class ItemStackMixin {
+    @Inject(method = "useOnBlock(Lnet/minecraft/item/ItemUsageContext;)Lnet/minecraft/util/ActionResult;", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/Item;useOnBlock(Lnet/minecraft/item/ItemUsageContext;)Lnet/minecraft/util/ActionResult;"))
+    private void cacheState(
+        ItemUsageContext context,
+        CallbackInfoReturnable<ActionResult> cir,
+        @Local Item item,
+        @Share("place") LocalRef<ItemPlacementContext> place
+    ) {
+        if (item instanceof BlockItem) {
+            World world = context.getWorld();
+            if (!world.isClient) {
+                place.set(new ItemPlacementContext(context));
+            }
+        }
+    }
+
+    @Inject(method = "useOnBlock(Lnet/minecraft/item/ItemUsageContext;)Lnet/minecraft/util/ActionResult;", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/ActionResult$Success;shouldIncrementStat()Z"))
+    private void useOnBlock(
+        ItemUsageContext context,
+        CallbackInfoReturnable<ActionResult> cir,
+        @Local PlayerEntity player,
+        @Share("place") LocalRef<ItemPlacementContext> place
+    ) {
+        ItemPlacementContext placementContext = place.get();
+        if (placementContext != null) {
+            ServerWorld world = (ServerWorld) context.getWorld();
+            BlockPos pos = placementContext.getBlockPos();
+            SuperGlueHandler.glueListensForBlockPlacement(world, player, pos);
+            if (!context.getStack().components.contains(DataComponentTypes.CONSUMABLE)) {
+                SymmetryHandler.onBlockPlaced(world, player, pos, placementContext);
+            }
+        }
+    }
+}
