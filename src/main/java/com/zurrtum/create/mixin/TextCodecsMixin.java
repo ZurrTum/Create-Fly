a780dc946cd69078f37ce43c514365a57ae401a2
ZurrTum <zurrtum@foxmail.com>
2025-10-05 16:52:57 +0800

    TextCodecs using registerTypes

--- src/main/java/com/zurrtum/create/mixin/TextCodecsMixin.java
+++ src/main/java/com/zurrtum/create/mixin/TextCodecsMixin.java
@@ -1,31 +1,19 @@
 package com.zurrtum.create.mixin;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.mojang.serialization.MapCodec;
 import com.zurrtum.create.foundation.recipe.IngredientTextContent;
 import net.minecraft.text.TextCodecs;
 import net.minecraft.text.TextContent;
-import net.minecraft.util.StringIdentifiable;
+import net.minecraft.util.dynamic.Codecs;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Function;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TextCodecs.class)
 public class TextCodecsMixin {
-    @WrapOperation(method = "createCodec(Lcom/mojang/serialization/Codec;)Lcom/mojang/serialization/Codec;", at = @At(value = "INVOKE", target = "Lnet/minecraft/text/TextCodecs;dispatchingCodec([Lnet/minecraft/util/StringIdentifiable;Ljava/util/function/Function;Ljava/util/function/Function;Ljava/lang/String;)Lcom/mojang/serialization/MapCodec;"))
-    private static MapCodec<TextContent> addType(
-        StringIdentifiable[] types,
-        Function<StringIdentifiable, MapCodec<TextContent>> typeToCodec,
-        Function<TextContent, StringIdentifiable> valueToType,
-        String dispatchingKey,
-        Operation<MapCodec<TextContent>> original
-    ) {
-        ArrayList<StringIdentifiable> list = new ArrayList<>(List.of(types));
-        list.add(IngredientTextContent.TYPE);
-        return original.call(list.toArray(StringIdentifiable[]::new), typeToCodec, valueToType, dispatchingKey);
+    @Inject(method = "registerTypes(Lnet/minecraft/util/dynamic/Codecs$IdMapper;)V", at = @At("TAIL"))
+    private static void registerTypes(Codecs.IdMapper<String, MapCodec<? extends TextContent>> idMapper, CallbackInfo ci) {
+        idMapper.put("ingredient", IngredientTextContent.CODEC);
     }
 }
