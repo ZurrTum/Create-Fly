cffbc455d231fe0fb1ae9851c20fb4a8038a16f3
ZurrTum <zurrtum@foxmail.com>
2025-10-08 10:36:57 +0800

    ParticleManager -> ParticleSpriteManager

--- src/client/java/com/zurrtum/create/client/AllParticleTypes.java
+++ src/client/java/com/zurrtum/create/client/AllParticleTypes.java
@@ -1,24 +1,24 @@
 package com.zurrtum.create.client;
 
 import com.zurrtum.create.client.infrastructure.particle.*;
-import net.minecraft.client.particle.ParticleManager;
+import net.minecraft.client.particle.ParticleSpriteManager;
 
 import static com.zurrtum.create.AllParticleTypes.*;
 
 public class AllParticleTypes {
-    public static void register(ParticleManager particle) {
-        particle.registerFactory(ROTATION_INDICATOR, RotationIndicatorParticle.Factory::new);
-        particle.registerFactory(AIR_FLOW, AirFlowParticle.Factory::new);
-        particle.registerFactory(AIR, AirParticle.Factory::new);
-        particle.registerFactory(STEAM_JET, SteamJetParticle.Factory::new);
-        particle.registerFactory(CUBE, new CubeParticle.Factory());
-        particle.registerFactory(FLUID_PARTICLE, new FluidParticle.Factory());
-        particle.registerFactory(FLUID_DRIP, new FluidParticle.Factory());
-        particle.registerFactory(BASIN_FLUID, new BasinFluidParticle.Factory());
-        particle.registerFactory(WIFI, BasicParticleFactory::wifi);
-        particle.registerFactory(SOUL, BasicParticleFactory::soul);
-        particle.registerFactory(SOUL_BASE, BasicParticleFactory::soulBase);
-        particle.registerFactory(SOUL_PERIMETER, BasicParticleFactory::soul);
-        particle.registerFactory(SOUL_EXPANDING_PERIMETER, BasicParticleFactory::soul);
+    public static void register(ParticleSpriteManager particle) {
+        particle.register(ROTATION_INDICATOR, RotationIndicatorParticle.Factory::new);
+        particle.register(AIR_FLOW, AirFlowParticle.Factory::new);
+        particle.register(AIR, AirParticle.Factory::new);
+        particle.register(STEAM_JET, SteamJetParticle.Factory::new);
+        particle.register(CUBE, new CubeParticle.Factory());
+        particle.register(FLUID_PARTICLE, new FluidParticle.Factory());
+        particle.register(FLUID_DRIP, new FluidParticle.Factory());
+        particle.register(BASIN_FLUID, new BasinFluidParticle.Factory());
+        particle.register(WIFI, BasicParticleFactory::wifi);
+        particle.register(SOUL, BasicParticleFactory::soul);
+        particle.register(SOUL_BASE, BasicParticleFactory::soulBase);
+        particle.register(SOUL_PERIMETER, BasicParticleFactory::soul);
+        particle.register(SOUL_EXPANDING_PERIMETER, BasicParticleFactory::soul);
     }
 }
