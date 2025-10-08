6bb8e138fae0fefb23b749f6a0f41e761f1f19f5
ZurrTum <zurrtum@foxmail.com>
2025-10-09 00:18:42 +0800

    CreativeMotorRenderer

--- src/client/java/com/zurrtum/create/client/content/kinetics/motor/CreativeMotorRenderer.java
+++ src/client/java/com/zurrtum/create/client/content/kinetics/motor/CreativeMotorRenderer.java
@@ -4,19 +4,17 @@ import com.zurrtum.create.client.AllPartialModels;
 import com.zurrtum.create.client.catnip.render.CachedBuffers;
 import com.zurrtum.create.client.catnip.render.SuperByteBuffer;
 import com.zurrtum.create.client.content.kinetics.base.KineticBlockEntityRenderer;
+import com.zurrtum.create.client.content.kinetics.base.KineticBlockEntityRenderer.KineticRenderState;
 import com.zurrtum.create.content.kinetics.motor.CreativeMotorBlockEntity;
-import net.minecraft.block.BlockState;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
 
-public class CreativeMotorRenderer extends KineticBlockEntityRenderer<CreativeMotorBlockEntity> {
-
+public class CreativeMotorRenderer extends KineticBlockEntityRenderer<CreativeMotorBlockEntity, KineticRenderState> {
     public CreativeMotorRenderer(BlockEntityRendererFactory.Context context) {
         super(context);
     }
 
     @Override
-    protected SuperByteBuffer getRotatedModel(CreativeMotorBlockEntity be, BlockState state) {
-        return CachedBuffers.partialFacing(AllPartialModels.SHAFT_HALF, state);
+    protected SuperByteBuffer getRotatedModel(CreativeMotorBlockEntity be, KineticRenderState state) {
+        return CachedBuffers.partialFacing(AllPartialModels.SHAFT_HALF, state.blockState);
     }
-
 }
