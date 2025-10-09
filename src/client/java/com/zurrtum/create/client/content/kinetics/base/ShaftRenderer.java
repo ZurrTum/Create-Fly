51173e51ee891f504a8a5afc7c7bcfcbacfd2508
ZurrTum <zurrtum@foxmail.com>
2025-10-09 11:00:26 +0800

    ShaftRenderer

--- src/client/java/com/zurrtum/create/client/content/kinetics/base/ShaftRenderer.java
+++ src/client/java/com/zurrtum/create/client/content/kinetics/base/ShaftRenderer.java
@@ -1,10 +1,11 @@
 package com.zurrtum.create.client.content.kinetics.base;
 
+import com.zurrtum.create.client.content.kinetics.base.KineticBlockEntityRenderer.KineticRenderState;
 import com.zurrtum.create.content.kinetics.base.KineticBlockEntity;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
 
-public class ShaftRenderer<T extends KineticBlockEntity> extends KineticBlockEntityRenderer<T> {
+public class ShaftRenderer<T extends KineticBlockEntity> extends KineticBlockEntityRenderer<T, KineticRenderState> {
 
     public ShaftRenderer(BlockEntityRendererFactory.Context context) {
         super(context);
