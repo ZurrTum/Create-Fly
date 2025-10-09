d584202904f186733d75b479811de50eab99a8cb
ZurrTum <zurrtum@foxmail.com>
2025-10-10 03:38:27 +0800

    PumpRenderer

--- src/client/java/com/zurrtum/create/client/content/fluids/PumpRenderer.java
+++ src/client/java/com/zurrtum/create/client/content/fluids/PumpRenderer.java
@@ -4,19 +4,17 @@ import com.zurrtum.create.client.AllPartialModels;
 import com.zurrtum.create.client.catnip.render.CachedBuffers;
 import com.zurrtum.create.client.catnip.render.SuperByteBuffer;
 import com.zurrtum.create.client.content.kinetics.base.KineticBlockEntityRenderer;
+import com.zurrtum.create.client.content.kinetics.base.KineticBlockEntityRenderer.KineticRenderState;
 import com.zurrtum.create.content.fluids.pump.PumpBlockEntity;
-import net.minecraft.block.BlockState;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
 
-public class PumpRenderer extends KineticBlockEntityRenderer<PumpBlockEntity> {
-
+public class PumpRenderer extends KineticBlockEntityRenderer<PumpBlockEntity, KineticRenderState> {
     public PumpRenderer(BlockEntityRendererFactory.Context context) {
         super(context);
     }
 
     @Override
-    protected SuperByteBuffer getRotatedModel(PumpBlockEntity be, BlockState state) {
-        return CachedBuffers.partialFacing(AllPartialModels.MECHANICAL_PUMP_COG, state);
+    protected SuperByteBuffer getRotatedModel(PumpBlockEntity be, KineticRenderState state) {
+        return CachedBuffers.partialFacing(AllPartialModels.MECHANICAL_PUMP_COG, state.blockState);
     }
-
 }
