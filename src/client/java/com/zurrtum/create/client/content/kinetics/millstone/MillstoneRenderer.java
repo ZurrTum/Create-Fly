8f246c4f1dd8116ff6f1e15761e03bcd600c6fbc
ZurrTum <zurrtum@foxmail.com>
2025-10-11 14:32:50 +0800

    MillstoneRenderer

--- src/client/java/com/zurrtum/create/client/content/kinetics/millstone/MillstoneRenderer.java
+++ src/client/java/com/zurrtum/create/client/content/kinetics/millstone/MillstoneRenderer.java
@@ -4,19 +4,17 @@ import com.zurrtum.create.client.AllPartialModels;
 import com.zurrtum.create.client.catnip.render.CachedBuffers;
 import com.zurrtum.create.client.catnip.render.SuperByteBuffer;
 import com.zurrtum.create.client.content.kinetics.base.KineticBlockEntityRenderer;
+import com.zurrtum.create.client.content.kinetics.base.KineticBlockEntityRenderer.KineticRenderState;
 import com.zurrtum.create.content.kinetics.millstone.MillstoneBlockEntity;
-import net.minecraft.block.BlockState;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
 
-public class MillstoneRenderer extends KineticBlockEntityRenderer<MillstoneBlockEntity> {
-
+public class MillstoneRenderer extends KineticBlockEntityRenderer<MillstoneBlockEntity, KineticRenderState> {
     public MillstoneRenderer(BlockEntityRendererFactory.Context context) {
         super(context);
     }
 
     @Override
-    protected SuperByteBuffer getRotatedModel(MillstoneBlockEntity be, BlockState state) {
-        return CachedBuffers.partial(AllPartialModels.MILLSTONE_COG, state);
+    protected SuperByteBuffer getRotatedModel(MillstoneBlockEntity be, KineticRenderState state) {
+        return CachedBuffers.partial(AllPartialModels.MILLSTONE_COG, state.blockState);
     }
-
 }
