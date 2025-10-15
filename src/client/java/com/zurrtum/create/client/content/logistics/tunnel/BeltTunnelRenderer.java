5906cd582a4dedc110b13fdd143d4ac3d8c05396
ZurrTum <zurrtum@foxmail.com>
2025-10-16 04:18:40 +0800

    BeltTunnelRenderer

--- src/client/java/com/zurrtum/create/client/content/logistics/tunnel/BeltTunnelRenderer.java
+++ src/client/java/com/zurrtum/create/client/content/logistics/tunnel/BeltTunnelRenderer.java
@@ -5,41 +5,69 @@ import com.zurrtum.create.client.AllPartialModels;
 import com.zurrtum.create.client.catnip.render.CachedBuffers;
 import com.zurrtum.create.client.catnip.render.SuperByteBuffer;
 import com.zurrtum.create.client.content.logistics.FlapStuffs;
+import com.zurrtum.create.client.content.logistics.FlapStuffs.FlapsRenderState;
 import com.zurrtum.create.client.flywheel.api.visualization.VisualizationManager;
 import com.zurrtum.create.client.foundation.blockEntity.renderer.SmartBlockEntityRenderer;
 import com.zurrtum.create.content.logistics.tunnel.BeltTunnelBlockEntity;
 import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.render.VertexConsumer;
-import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
+import net.minecraft.client.render.command.ModelCommandRenderer;
+import net.minecraft.client.render.command.OrderedRenderCommandQueue;
+import net.minecraft.client.render.state.CameraRenderState;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
+import org.jetbrains.annotations.Nullable;
 
-public class BeltTunnelRenderer extends SmartBlockEntityRenderer<BeltTunnelBlockEntity> {
+import java.util.ArrayList;
+import java.util.List;
 
+public class BeltTunnelRenderer extends SmartBlockEntityRenderer<BeltTunnelBlockEntity, BeltTunnelRenderer.BeltTunnelRenderState> {
     public BeltTunnelRenderer(BlockEntityRendererFactory.Context context) {
         super(context);
     }
 
     @Override
-    protected void renderSafe(BeltTunnelBlockEntity be, float partialTicks, MatrixStack ms, VertexConsumerProvider buffer, int light, int overlay) {
-        super.renderSafe(be, partialTicks, ms, buffer, light, overlay);
-
-        if (VisualizationManager.supportsVisualization(be.getWorld()))
-            return;
-
-        SuperByteBuffer flapBuffer = CachedBuffers.partial(AllPartialModels.BELT_TUNNEL_FLAP, be.getCachedState());
-        VertexConsumer vb = buffer.getBuffer(RenderLayer.getSolid());
-
-        for (Direction direction : Iterate.directions) {
-            if (!be.flaps.containsKey(direction))
-                continue;
-
-            float f = be.flaps.get(direction).getValue(partialTicks);
-
-            FlapStuffs.renderFlaps(ms, vb, flapBuffer, FlapStuffs.TUNNEL_PIVOT, direction, f, 0, light);
-        }
-
+    public BeltTunnelRenderState createRenderState() {
+        return new BeltTunnelRenderState();
     }
 
+    @Override
+    public void updateRenderState(
+        BeltTunnelBlockEntity be,
+        BeltTunnelRenderState state,
+        float tickProgress,
+        Vec3d cameraPos,
+        ModelCommandRenderer.@Nullable CrumblingOverlayCommand crumblingOverlay
+    ) {
+        super.updateRenderState(be, state, tickProgress, cameraPos, crumblingOverlay);
+        if (VisualizationManager.supportsVisualization(be.getWorld())) {
+            return;
+        }
+        SuperByteBuffer flapBuffer = CachedBuffers.partial(AllPartialModels.BELT_TUNNEL_FLAP, state.blockState);
+        List<FlapsRenderState> flaps = new ArrayList<>();
+        for (Direction direction : Iterate.directions) {
+            if (!be.flaps.containsKey(direction)) {
+                continue;
+            }
+            float f = be.flaps.get(direction).getValue(tickProgress);
+            flaps.add(FlapStuffs.getFlapsRenderState(flapBuffer, FlapStuffs.TUNNEL_PIVOT, direction, f, 0, state.lightmapCoordinates));
+        }
+        state.flaps = flaps;
+    }
+
+    @Override
+    public void render(BeltTunnelRenderState state, MatrixStack matrices, OrderedRenderCommandQueue queue, CameraRenderState cameraState) {
+        super.render(state, matrices, queue, cameraState);
+        if (state.flaps != null) {
+            RenderLayer layer = RenderLayer.getSolid();
+            for (FlapsRenderState flap : state.flaps) {
+                flap.render(layer, matrices, queue);
+            }
+        }
+    }
+
+    public static class BeltTunnelRenderState extends SmartRenderState {
+        public List<FlapsRenderState> flaps;
+    }
 }
