02fababa1c709b825474ced7c9e1ded61e0a80ce
ZurrTum <zurrtum@foxmail.com>
2025-10-16 02:51:20 +0800

    BasinRenderer

--- src/client/java/com/zurrtum/create/client/content/processing/basin/BasinRenderer.java
+++ src/client/java/com/zurrtum/create/client/content/processing/basin/BasinRenderer.java
@@ -5,7 +5,7 @@ import com.zurrtum.create.catnip.math.AngleHelper;
 import com.zurrtum.create.catnip.math.VecHelper;
 import com.zurrtum.create.client.catnip.animation.AnimationTickHolder;
 import com.zurrtum.create.client.catnip.render.FluidRenderHelper;
-import com.zurrtum.create.client.flywheel.lib.transform.TransformStack;
+import com.zurrtum.create.client.catnip.render.PonderRenderTypes;
 import com.zurrtum.create.client.foundation.blockEntity.renderer.SmartBlockEntityRenderer;
 import com.zurrtum.create.content.processing.basin.BasinBlock;
 import com.zurrtum.create.content.processing.basin.BasinBlockEntity;
@@ -14,142 +14,100 @@ import com.zurrtum.create.foundation.blockEntity.behaviour.fluid.SmartFluidTankB
 import com.zurrtum.create.foundation.blockEntity.behaviour.fluid.SmartFluidTankBehaviour.TankSegment;
 import com.zurrtum.create.infrastructure.fluids.BucketFluidInventory;
 import com.zurrtum.create.infrastructure.fluids.FluidStack;
-import net.minecraft.block.BlockState;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.OverlayTexture;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
+import net.minecraft.client.render.command.ModelCommandRenderer;
+import net.minecraft.client.render.command.OrderedRenderCommandQueue;
+import net.minecraft.client.render.item.ItemRenderState;
+import net.minecraft.client.render.state.CameraRenderState;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.component.ComponentChanges;
+import net.minecraft.fluid.Fluid;
 import net.minecraft.item.ItemDisplayContext;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.*;
 import net.minecraft.util.math.Direction.Axis;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
-public class BasinRenderer extends SmartBlockEntityRenderer<BasinBlockEntity> {
+import java.util.ArrayList;
+import java.util.List;
 
+public class BasinRenderer extends SmartBlockEntityRenderer<BasinBlockEntity, BasinRenderer.BasinRenderState> {
     public BasinRenderer(BlockEntityRendererFactory.Context context) {
         super(context);
     }
 
     @Override
-    protected void renderSafe(BasinBlockEntity basin, float partialTicks, MatrixStack ms, VertexConsumerProvider buffer, int light, int overlay) {
-        super.renderSafe(basin, partialTicks, ms, buffer, light, overlay);
+    public BasinRenderState createRenderState() {
+        return new BasinRenderState();
+    }
 
-        float fluidLevel = renderFluids(basin, partialTicks, ms, buffer, light, overlay);
-        float level = MathHelper.clamp(fluidLevel - .3f, .125f, .6f);
+    @Override
+    public void updateRenderState(
+        BasinBlockEntity be,
+        BasinRenderState state,
+        float tickProgress,
+        Vec3d cameraPos,
+        @Nullable ModelCommandRenderer.CrumblingOverlayCommand crumblingOverlay
+    ) {
+        super.updateRenderState(be, state, tickProgress, cameraPos, crumblingOverlay);
+        float fluidLevel = updateFluids(be, state, tickProgress);
+        updateIngredients(be, state, tickProgress, fluidLevel);
+        updateOutputs(be, state, tickProgress);
+    }
 
-        ms.push();
-
-        BlockPos pos = basin.getPos();
-        ms.translate(.5, .2f, .5);
-        TransformStack.of(ms).rotateYDegrees(basin.ingredientRotation.getValue(partialTicks));
-
-        Random r = Random.create(pos.hashCode());
-        Vec3d baseVector = new Vec3d(.125, level, 0);
-
-        World world = basin.getWorld();
-        BasinInventory inv = basin.itemCapability;
-        if (inv != null) {
-            int size = inv.size();
-            int itemCount = 0;
-            for (int slot = 0; slot < size; slot++)
-                if (!inv.getStack(slot).isEmpty())
-                    itemCount++;
-
-            if (itemCount == 1)
-                baseVector = new Vec3d(0, level, 0);
-
-            float anglePartition = 360f / itemCount;
-            for (int slot = 0; slot < size; slot++) {
-                ItemStack stack = inv.getStack(slot);
-                if (stack.isEmpty())
-                    continue;
-
-                ms.push();
-
-                if (fluidLevel > 0) {
-                    ms.translate(
-                        0,
-                        (MathHelper.sin(AnimationTickHolder.getRenderTime(world) / 12f + anglePartition * itemCount) + 1.5f) * 1 / 32f,
-                        0
-                    );
+    @Override
+    public void render(BasinRenderState state, MatrixStack matrices, OrderedRenderCommandQueue queue, CameraRenderState cameraState) {
+        super.render(state, matrices, queue, cameraState);
+        if (state.fluids != null) {
+            queue.submitCustom(matrices, state.layer, state);
+        }
+        if (state.ingredients != null) {
+            matrices.push();
+            matrices.translate(.5, .2f, .5);
+            matrices.multiply(RotationAxis.POSITIVE_Y.rotation(state.ingredientYRot));
+            for (IngredientRenderData ingredient : state.ingredients) {
+                matrices.push();
+                matrices.translate(ingredient.itemPosition);
+                matrices.multiply(RotationAxis.POSITIVE_Y.rotation(ingredient.yRot));
+                matrices.multiply(RotationAxis.POSITIVE_X.rotation(state.ingredientXRot));
+                for (Vec3d offset : ingredient.offsets) {
+                    matrices.push();
+                    matrices.translate(offset);
+                    ingredient.renderState.render(matrices, queue, state.lightmapCoordinates, OverlayTexture.DEFAULT_UV, 0);
+                    matrices.pop();
                 }
-
-                Vec3d itemPosition = VecHelper.rotate(baseVector, anglePartition * itemCount, Axis.Y);
-                ms.translate(itemPosition.x, itemPosition.y, itemPosition.z);
-                TransformStack.of(ms).rotateYDegrees(anglePartition * itemCount + 35).rotateXDegrees(65);
-
-                for (int i = 0; i <= stack.getCount() / 8; i++) {
-                    ms.push();
-
-                    Vec3d vec = VecHelper.offsetRandomly(Vec3d.ZERO, r, 1 / 16f);
-
-                    ms.translate(vec.x, vec.y, vec.z);
-                    renderItem(ms, buffer, light, overlay, stack);
-                    ms.pop();
-                }
-                ms.pop();
-
-                itemCount--;
+                matrices.pop();
+            }
+            matrices.pop();
+        }
+        if (state.outputs != null) {
+            for (OutputItemRenderData item : state.outputs) {
+                matrices.push();
+                matrices.translate(item.offset);
+                matrices.multiply(RotationAxis.POSITIVE_Y.rotation(state.outputYRot));
+                matrices.multiply(RotationAxis.POSITIVE_X.rotation(item.xRot));
+                item.renderState.render(matrices, queue, state.lightmapCoordinates, OverlayTexture.DEFAULT_UV, 0);
+                matrices.pop();
             }
         }
-        ms.pop();
-
-        BlockState blockState = basin.getCachedState();
-        if (!(blockState.getBlock() instanceof BasinBlock))
-            return;
-        Direction direction = blockState.get(BasinBlock.FACING);
-        if (direction == Direction.DOWN)
-            return;
-        Vec3d directionVec = Vec3d.of(direction.getVector());
-        Vec3d outVec = VecHelper.getCenterOf(BlockPos.ZERO).add(directionVec.multiply(.55).subtract(0, 1 / 2f, 0));
-
-        boolean outToBasin = world.getBlockState(basin.getPos().offset(direction)).getBlock() instanceof BasinBlock;
-
-        for (IntAttached<ItemStack> intAttached : basin.visualizedOutputItems) {
-            float progress = 1 - (intAttached.getFirst() - partialTicks) / BasinBlockEntity.OUTPUT_ANIMATION_TIME;
-
-            if (!outToBasin && progress > .35f)
-                continue;
-
-            ms.push();
-            TransformStack.of(ms).translate(outVec).translate(new Vec3d(0, Math.max(-.55f, -(progress * progress * 2)), 0))
-                .translate(directionVec.multiply(progress * .5f)).rotateYDegrees(AngleHelper.horizontalAngle(direction))
-                .rotateXDegrees(progress * 180);
-            renderItem(ms, buffer, light, overlay, intAttached.getValue());
-            ms.pop();
-        }
     }
 
-    protected void renderItem(MatrixStack ms, VertexConsumerProvider buffer, int light, int overlay, ItemStack stack) {
-        MinecraftClient mc = MinecraftClient.getInstance();
-        mc.getItemRenderer().renderItem(stack, ItemDisplayContext.GROUND, light, overlay, ms, buffer, mc.world, 0);
-    }
-
-    protected float renderFluids(BasinBlockEntity basin, float partialTicks, MatrixStack ms, VertexConsumerProvider buffer, int light, int overlay) {
-        SmartFluidTankBehaviour inputFluids = basin.getBehaviour(SmartFluidTankBehaviour.INPUT);
-        SmartFluidTankBehaviour outputFluids = basin.getBehaviour(SmartFluidTankBehaviour.OUTPUT);
-        SmartFluidTankBehaviour[] tanks = {inputFluids, outputFluids};
+    public float updateFluids(BasinBlockEntity basin, BasinRenderState state, float partialTicks) {
         float totalUnits = basin.getTotalFluidUnits(partialTicks);
         if (totalUnits < 1)
             return 0;
-
-        float fluidLevel = MathHelper.clamp(totalUnits / (BucketFluidInventory.CAPACITY * 2), 0, 1);
-
-        fluidLevel = 1 - ((1 - fluidLevel) * (1 - fluidLevel));
-
         float xMin = 2 / 16f;
         float xMax = 2 / 16f;
-        final float yMin = 2 / 16f;
-        final float yMax = yMin + 12 / 16f * fluidLevel;
-        final float zMin = 2 / 16f;
-        final float zMax = 14 / 16f;
-
-        for (SmartFluidTankBehaviour behaviour : tanks) {
+        List<FluidRenderData> fluids = new ArrayList<>();
+        for (SmartFluidTankBehaviour behaviour : List.of(
+            basin.getBehaviour(SmartFluidTankBehaviour.INPUT),
+            basin.getBehaviour(SmartFluidTankBehaviour.OUTPUT)
+        )) {
             if (behaviour == null)
                 continue;
             for (TankSegment tankSegment : behaviour.getTanks()) {
@@ -162,13 +120,106 @@ public class BasinRenderer extends SmartBlockEntityRenderer<BasinBlockEntity> {
 
                 float partial = MathHelper.clamp(units / totalUnits, 0, 1);
                 xMax += partial * 12 / 16f;
-                FluidRenderHelper.renderFluidBox(renderedFluid, xMin, yMin, zMin, xMax, yMax, zMax, buffer, ms, light, false, false);
+                fluids.add(new FluidRenderData(renderedFluid.getFluid(), renderedFluid.getComponentChanges(), xMin, xMax));
 
                 xMin = xMax;
             }
         }
+        if (fluids.isEmpty()) {
+            return 0;
+        }
+        float fluidLevel = MathHelper.clamp(totalUnits / (BucketFluidInventory.CAPACITY * 2), 0, 1);
+        fluidLevel = 1 - ((1 - fluidLevel) * (1 - fluidLevel));
+        state.layer = PonderRenderTypes.fluid();
+        state.fluids = fluids;
+        state.yMin = 2 / 16f;
+        state.yMax = state.yMin + 12 / 16f * fluidLevel;
+        state.zMin = 2 / 16f;
+        state.zMax = 14 / 16f;
+        return state.yMax;
+    }
 
-        return yMax;
+    public void updateIngredients(BasinBlockEntity be, BasinRenderState state, float partialTicks, float fluidLevel) {
+        BasinInventory inv = be.itemCapability;
+        if (inv == null) {
+            return;
+        }
+        List<ItemStack> stacks = new ArrayList<>();
+        for (int slot = 0, size = inv.size(); slot < size; slot++) {
+            ItemStack stack = inv.getStack(slot);
+            if (stack.isEmpty()) {
+                continue;
+            }
+            stacks.add(stack);
+        }
+        int itemCount = stacks.size();
+        if (itemCount == 0) {
+            return;
+        }
+        float level = MathHelper.clamp(fluidLevel - .3f, .125f, .6f);
+        Random r = Random.create(state.pos.hashCode());
+        Vec3d baseVector = new Vec3d(itemCount == 1 ? 0 : .125, level, 0);
+        World world = be.getWorld();
+        float time = AnimationTickHolder.getRenderTime(world);
+        float anglePartition = 360f / itemCount;
+        IngredientRenderData[] ingredients = new IngredientRenderData[itemCount];
+        for (int i = 0, size = itemCount; i < size; i++) {
+            ItemStack stack = stacks.get(i);
+            Vec3d itemPosition = VecHelper.rotate(baseVector, anglePartition * itemCount, Axis.Y);
+            if (fluidLevel > 0) {
+                itemPosition = itemPosition.add(0, (MathHelper.sin(time / 12f + anglePartition * itemCount) + 1.5f) * 1 / 32f, 0);
+            }
+            float yRot = MathHelper.RADIANS_PER_DEGREE * (anglePartition * itemCount + 35);
+            ItemRenderState renderState = new ItemRenderState();
+            renderState.displayContext = ItemDisplayContext.GROUND;
+            itemModelManager.update(renderState, stack, renderState.displayContext, world, null, 0);
+            int count = stack.getCount() / 8 + 1;
+            Vec3d[] offsets = new Vec3d[count];
+            for (int j = 0; j < count; j++) {
+                offsets[j] = VecHelper.offsetRandomly(Vec3d.ZERO, r, 1 / 16f);
+            }
+            ingredients[i] = new IngredientRenderData(renderState, itemPosition, yRot, offsets);
+            itemCount--;
+        }
+        state.ingredientYRot = MathHelper.RADIANS_PER_DEGREE * be.ingredientRotation.getValue(partialTicks);
+        state.ingredientXRot = MathHelper.RADIANS_PER_DEGREE * 65;
+        state.ingredients = ingredients;
+    }
+
+    private void updateOutputs(BasinBlockEntity be, BasinRenderState state, float partialTicks) {
+        if (!(state.blockState.getBlock() instanceof BasinBlock)) {
+            return;
+        }
+        Direction direction = state.blockState.get(BasinBlock.FACING);
+        if (direction == Direction.DOWN) {
+            return;
+        }
+        List<IntAttached<ItemStack>> visualizedOutputItems = be.visualizedOutputItems;
+        if (visualizedOutputItems.isEmpty()) {
+            return;
+        }
+        Vec3d directionVec = Vec3d.of(direction.getVector());
+        Vec3d outVec = VecHelper.getCenterOf(BlockPos.ZERO).add(directionVec.multiply(.55).subtract(0, 1 / 2f, 0));
+        World world = be.getWorld();
+        boolean outToBasin = world.getBlockState(state.pos.offset(direction)).getBlock() instanceof BasinBlock;
+        List<OutputItemRenderData> outputs = new ArrayList<>();
+        for (IntAttached<ItemStack> intAttached : visualizedOutputItems) {
+            float progress = 1 - (intAttached.getFirst() - partialTicks) / BasinBlockEntity.OUTPUT_ANIMATION_TIME;
+            if (!outToBasin && progress > .35f) {
+                continue;
+            }
+            Vec3d offset = outVec.add(0, Math.max(-.55f, -(progress * progress * 2)), 0).add(directionVec.multiply(progress * .5f));
+            float xRot = MathHelper.RADIANS_PER_DEGREE * progress * 180;
+            ItemRenderState renderState = new ItemRenderState();
+            renderState.displayContext = ItemDisplayContext.GROUND;
+            itemModelManager.update(renderState, intAttached.getValue(), renderState.displayContext, world, null, 0);
+            outputs.add(new OutputItemRenderData(renderState, offset, xRot));
+        }
+        if (outputs.isEmpty()) {
+            return;
+        }
+        state.outputYRot = MathHelper.RADIANS_PER_DEGREE * AngleHelper.horizontalAngle(direction);
+        state.outputs = outputs;
     }
 
     @Override
@@ -176,4 +227,43 @@ public class BasinRenderer extends SmartBlockEntityRenderer<BasinBlockEntity> {
         return 16;
     }
 
+    public static class BasinRenderState extends SmartRenderState implements OrderedRenderCommandQueue.Custom {
+        public RenderLayer layer;
+        public float yMin, yMax, zMin, zMax;
+        public List<FluidRenderData> fluids;
+        public float ingredientYRot, ingredientXRot;
+        public IngredientRenderData[] ingredients;
+        public float outputYRot;
+        public List<OutputItemRenderData> outputs;
+
+        @Override
+        public void render(MatrixStack.Entry matricesEntry, VertexConsumer vertexConsumer) {
+            for (FluidRenderData data : fluids) {
+                FluidRenderHelper.renderFluidBox(
+                    data.fluid,
+                    data.changes,
+                    data.xMin,
+                    yMin,
+                    zMin,
+                    data.xMax,
+                    yMax,
+                    zMax,
+                    vertexConsumer,
+                    matricesEntry,
+                    lightmapCoordinates,
+                    false,
+                    false
+                );
+            }
+        }
+    }
+
+    public record FluidRenderData(Fluid fluid, ComponentChanges changes, float xMin, float xMax) {
+    }
+
+    public record IngredientRenderData(ItemRenderState renderState, Vec3d itemPosition, float yRot, Vec3d[] offsets) {
+    }
+
+    public record OutputItemRenderData(ItemRenderState renderState, Vec3d offset, float xRot) {
+    }
 }
--- src/client/java/com/zurrtum/create/client/foundation/blockEntity/renderer/SmartBlockEntityRenderer.java
+++ src/client/java/com/zurrtum/create/client/foundation/blockEntity/renderer/SmartBlockEntityRenderer.java
@@ -1,53 +1,92 @@
 package com.zurrtum.create.client.foundation.blockEntity.renderer;
 
+import com.zurrtum.create.catnip.math.VecHelper;
 import com.zurrtum.create.client.content.redstone.link.LinkRenderer;
+import com.zurrtum.create.client.content.redstone.link.LinkRenderer.LinkRenderState;
 import com.zurrtum.create.client.foundation.blockEntity.behaviour.filtering.FilteringRenderer;
+import com.zurrtum.create.client.foundation.blockEntity.behaviour.filtering.FilteringRenderer.FilterRenderState;
 import com.zurrtum.create.foundation.blockEntity.SmartBlockEntity;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.item.ItemModelManager;
 import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
+import net.minecraft.client.render.block.entity.state.BlockEntityRenderState;
+import net.minecraft.client.render.command.ModelCommandRenderer;
+import net.minecraft.client.render.command.OrderedRenderCommandQueue;
+import net.minecraft.client.render.state.CameraRenderState;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
-import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.hit.HitResult;
-import net.minecraft.util.hit.HitResult.Type;
 import net.minecraft.util.math.Vec3d;
-import org.joml.Matrix4f;
+import org.jetbrains.annotations.Nullable;
 
-public class SmartBlockEntityRenderer<T extends SmartBlockEntity> extends SafeBlockEntityRenderer<T> {
+public class SmartBlockEntityRenderer<T extends SmartBlockEntity, S extends SmartBlockEntityRenderer.SmartRenderState> implements BlockEntityRenderer<T, S> {
+    protected final ItemModelManager itemModelManager;
+
     public SmartBlockEntityRenderer(BlockEntityRendererFactory.Context context) {
+        itemModelManager = context.itemModelManager();
     }
 
     @Override
-    protected void renderSafe(T blockEntity, float partialTicks, MatrixStack ms, VertexConsumerProvider buffer, int light, int overlay) {
-        FilteringRenderer.renderOnBlockEntity(blockEntity, partialTicks, ms, buffer, light, overlay);
-        LinkRenderer.renderOnBlockEntity(blockEntity, partialTicks, ms, buffer, light, overlay);
+    @SuppressWarnings("unchecked")
+    public S createRenderState() {
+        return (S) new SmartRenderState();
+    }
+
+    @Override
+    public void updateRenderState(
+        T be,
+        S state,
+        float tickProgress,
+        Vec3d cameraPos,
+        @Nullable ModelCommandRenderer.CrumblingOverlayCommand crumblingOverlay
+    ) {
+        BlockEntityRenderState.updateBlockEntityRenderState(be, state, crumblingOverlay);
+        if (be.isRemoved()) {
+            return;
+        }
+        double distance = be.isVirtual() ? -1 : cameraPos.squaredDistanceTo(VecHelper.getCenterOf(state.pos));
+        state.filter = FilteringRenderer.getFilterRenderState(be, state.blockState, itemModelManager, distance);
+        state.link = LinkRenderer.getLinkRenderState(be, itemModelManager, distance);
+    }
+
+    @Override
+    public void render(S state, MatrixStack matrices, OrderedRenderCommandQueue queue, CameraRenderState cameraState) {
+        if (state.filter != null) {
+            state.filter.render(state.blockState, queue, matrices, state.lightmapCoordinates);
+        }
+        if (state.link != null) {
+            state.link.render(state.blockState, queue, matrices, state.lightmapCoordinates);
+        }
     }
 
     protected void renderNameplateOnHover(T blockEntity, Text tag, float yOffset, MatrixStack ms, VertexConsumerProvider buffer, int light) {
-        MinecraftClient mc = MinecraftClient.getInstance();
-        if (blockEntity.isVirtual())
-            return;
-        if (mc.player.squaredDistanceTo(Vec3d.ofCenter(blockEntity.getPos())) > 4096.0f)
-            return;
-        HitResult hitResult = mc.crosshairTarget;
-        if (!(hitResult instanceof BlockHitResult bhr) || bhr.getType() == Type.MISS || !bhr.getBlockPos().equals(blockEntity.getPos()))
-            return;
-
-        float f = yOffset + 0.25f;
-        ms.push();
-        ms.translate(0.5, f, 0.5);
-        ms.multiply(mc.getEntityRenderDispatcher().getRotation());
-        ms.scale(0.025F, -0.025F, 0.025F);
-        Matrix4f matrix4f = ms.peek().getPositionMatrix();
-        float f2 = mc.options.getTextBackgroundOpacity(0.25F);
-        int j = (int) (f2 * 255.0F) << 24;
-        TextRenderer font = mc.textRenderer;
-        float f1 = (float) (-font.getWidth(tag) / 2);
-        font.draw(tag, f1, (float) 0, 553648127, false, matrix4f, buffer, TextRenderer.TextLayerType.SEE_THROUGH, j, light);
-        font.draw(tag, f1, (float) 0, -1, false, matrix4f, buffer, TextRenderer.TextLayerType.NORMAL, 0, light);
-        ms.pop();
+        //TODO
+        //        MinecraftClient mc = MinecraftClient.getInstance();
+        //        if (blockEntity.isVirtual())
+        //            return;
+        //        if (mc.player.squaredDistanceTo(Vec3d.ofCenter(blockEntity.getPos())) > 4096.0f)
+        //            return;
+        //        HitResult hitResult = mc.crosshairTarget;
+        //        if (!(hitResult instanceof BlockHitResult bhr) || bhr.getType() == Type.MISS || !bhr.getBlockPos().equals(blockEntity.getPos()))
+        //            return;
+        //
+        //        float f = yOffset + 0.25f;
+        //        ms.push();
+        //        ms.translate(0.5, f, 0.5);
+        //        ms.multiply(mc.getEntityRenderDispatcher().getRotation());
+        //        ms.scale(0.025F, -0.025F, 0.025F);
+        //        Matrix4f matrix4f = ms.peek().getPositionMatrix();
+        //        float f2 = mc.options.getTextBackgroundOpacity(0.25F);
+        //        int j = (int) (f2 * 255.0F) << 24;
+        //        TextRenderer font = mc.textRenderer;
+        //        float f1 = (float) (-font.getWidth(tag) / 2);
+        //        font.draw(tag, f1, (float) 0, 553648127, false, matrix4f, buffer, TextRenderer.TextLayerType.SEE_THROUGH, j, light);
+        //        font.draw(tag, f1, (float) 0, -1, false, matrix4f, buffer, TextRenderer.TextLayerType.NORMAL, 0, light);
+        //        ms.pop();
     }
 
+    public static class SmartRenderState extends BlockEntityRenderState {
+        public FilterRenderState filter;
+        public LinkRenderState link;
+    }
 }
