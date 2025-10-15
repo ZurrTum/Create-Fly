fe102e101b7daf99e04570ce31b7657e999e2deb
ZurrTum <zurrtum@foxmail.com>
2025-10-16 04:43:07 +0800

    ChuteRenderer

--- src/client/java/com/zurrtum/create/client/content/logistics/chute/ChuteRenderer.java
+++ src/client/java/com/zurrtum/create/client/content/logistics/chute/ChuteRenderer.java
@@ -1,55 +1,106 @@
 package com.zurrtum.create.client.content.logistics.chute;
 
-import com.zurrtum.create.client.flywheel.lib.transform.TransformStack;
-import com.zurrtum.create.client.foundation.blockEntity.renderer.SafeBlockEntityRenderer;
 import com.zurrtum.create.content.logistics.box.PackageItem;
 import com.zurrtum.create.content.logistics.chute.ChuteBlock;
 import com.zurrtum.create.content.logistics.chute.ChuteBlock.Shape;
 import com.zurrtum.create.content.logistics.chute.ChuteBlockEntity;
-import net.minecraft.block.BlockState;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.item.ItemModelManager;
+import net.minecraft.client.render.OverlayTexture;
+import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
-import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.render.block.entity.state.BlockEntityRenderState;
+import net.minecraft.client.render.command.ModelCommandRenderer;
+import net.minecraft.client.render.command.OrderedRenderCommandQueue;
+import net.minecraft.client.render.item.ItemRenderState;
+import net.minecraft.client.render.state.CameraRenderState;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemDisplayContext;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.RotationAxis;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
-public class ChuteRenderer extends SafeBlockEntityRenderer<ChuteBlockEntity> {
+public class ChuteRenderer implements BlockEntityRenderer<ChuteBlockEntity, ChuteRenderer.ChuteRenderState> {
+    protected final ItemModelManager itemModelManager;
 
     public ChuteRenderer(BlockEntityRendererFactory.Context context) {
+        itemModelManager = context.itemModelManager();
     }
 
     @Override
-    protected void renderSafe(ChuteBlockEntity be, float partialTicks, MatrixStack ms, VertexConsumerProvider buffer, int light, int overlay) {
-        if (be.getItem().isEmpty())
-            return;
-        BlockState blockState = be.getCachedState();
-        if (blockState.get(ChuteBlock.FACING) != Direction.DOWN)
-            return;
-        if (blockState.get(ChuteBlock.SHAPE) != Shape.WINDOW && (be.bottomPullDistance == 0 || be.itemPosition.getValue(partialTicks) > .5f))
-            return;
-
-        renderItem(be, partialTicks, ms, buffer, light, overlay);
+    public ChuteRenderState createRenderState() {
+        return new ChuteRenderState();
     }
 
-    public static void renderItem(ChuteBlockEntity be, float partialTicks, MatrixStack ms, VertexConsumerProvider buffer, int light, int overlay) {
-        ItemRenderer itemRenderer = MinecraftClient.getInstance().getItemRenderer();
-        var msr = TransformStack.of(ms);
-        ms.push();
-        msr.center();
-        float itemScale = .5f;
-        float itemPosition = be.itemPosition.getValue(partialTicks);
-        ms.translate(0, -.5 + itemPosition, 0);
-        if (PackageItem.isPackage(be.getItem())) {
-            ms.scale(1.5f, 1.5f, 1.5f);
-        } else {
-            ms.scale(itemScale, itemScale, itemScale);
-            msr.rotateXDegrees(itemPosition * 180);
-            msr.rotateYDegrees(itemPosition * 180);
+    @Override
+    public void updateRenderState(
+        ChuteBlockEntity be,
+        ChuteRenderState state,
+        float tickProgress,
+        Vec3d cameraPos,
+        @Nullable ModelCommandRenderer.CrumblingOverlayCommand crumblingOverlay
+    ) {
+        BlockEntityRenderState.updateBlockEntityRenderState(be, state, crumblingOverlay);
+        ItemStack item = be.getItem();
+        if (item.isEmpty()) {
+            return;
         }
-        itemRenderer.renderItem(be.getItem(), ItemDisplayContext.FIXED, light, overlay, ms, buffer, be.getWorld(), 0);
-        ms.pop();
+        if (state.blockState.get(ChuteBlock.FACING) != Direction.DOWN) {
+            return;
+        }
+        boolean notWindow = state.blockState.get(ChuteBlock.SHAPE) != Shape.WINDOW;
+        if (notWindow && be.bottomPullDistance == 0) {
+            return;
+        }
+        float itemPosition = be.itemPosition.getValue(tickProgress);
+        if (notWindow && itemPosition > .5f) {
+            return;
+        }
+        state.item = ChuteItemRenderState.create(itemModelManager, item, itemPosition, be.getWorld());
     }
 
+    @Override
+    public void render(ChuteRenderState state, MatrixStack matrices, OrderedRenderCommandQueue queue, CameraRenderState cameraState) {
+        if (state.item != null) {
+            state.item.render(matrices, queue, state.lightmapCoordinates);
+        }
+    }
+
+    public static class ChuteRenderState extends BlockEntityRenderState {
+        public ChuteItemRenderState item;
+    }
+
+    public record ChuteItemRenderState(ItemRenderState item, float offset, float rotate) {
+        public static ChuteItemRenderState create(ItemModelManager itemModelManager, ItemStack stack, float itemPosition, World world) {
+            float offset = itemPosition - .5f;
+            float rotate;
+            if (PackageItem.isPackage(stack)) {
+                rotate = -1;
+            } else {
+                rotate = MathHelper.RADIANS_PER_DEGREE * itemPosition * 180;
+            }
+            ItemRenderState item = new ItemRenderState();
+            item.displayContext = ItemDisplayContext.FIXED;
+            itemModelManager.update(item, stack, item.displayContext, world, null, 0);
+            return new ChuteItemRenderState(item, offset, rotate);
+        }
+
+        public void render(MatrixStack matrices, OrderedRenderCommandQueue queue, int light) {
+            matrices.push();
+            matrices.translate(0.5f, 0.5f, 0.5f);
+            matrices.translate(0, offset, 0);
+            if (rotate == -1) {
+                matrices.scale(1.5f, 1.5f, 1.5f);
+            } else {
+                matrices.scale(0.5f, 0.5f, 0.5f);
+                matrices.multiply(RotationAxis.POSITIVE_X.rotation(rotate));
+                matrices.multiply(RotationAxis.POSITIVE_Y.rotation(rotate));
+            }
+            item.render(matrices, queue, light, OverlayTexture.DEFAULT_UV, 0);
+            matrices.pop();
+        }
+    }
 }
