6aac4e7c3f1dfdac4c384cf9e2407233a8648502
ZurrTum <zurrtum@foxmail.com>
2025-10-28 20:51:26 +0800

    ToolboxRenderer

--- src/client/java/com/zurrtum/create/client/content/equipment/toolbox/ToolboxRenderer.java
+++ src/client/java/com/zurrtum/create/client/content/equipment/toolbox/ToolboxRenderer.java
@@ -1,53 +1,74 @@
 package com.zurrtum.create.client.content.equipment.toolbox;
 
-import com.zurrtum.create.catnip.data.Iterate;
 import com.zurrtum.create.client.AllPartialModels;
 import com.zurrtum.create.client.catnip.render.CachedBuffers;
 import com.zurrtum.create.client.catnip.render.SuperByteBuffer;
-import com.zurrtum.create.client.foundation.blockEntity.renderer.SmartBlockEntityRenderer;
 import com.zurrtum.create.content.equipment.toolbox.ToolboxBlock;
 import com.zurrtum.create.content.equipment.toolbox.ToolboxBlockEntity;
-import net.minecraft.block.BlockState;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumer;
-import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
+import net.minecraft.client.render.block.entity.state.BlockEntityRenderState;
+import net.minecraft.client.render.command.ModelCommandRenderer;
+import net.minecraft.client.render.command.OrderedRenderCommandQueue;
+import net.minecraft.client.render.state.CameraRenderState;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
+import org.jetbrains.annotations.Nullable;
 
-public class ToolboxRenderer extends SmartBlockEntityRenderer<ToolboxBlockEntity> {
-
+public class ToolboxRenderer implements BlockEntityRenderer<ToolboxBlockEntity, ToolboxRenderer.ToolboxRenderState> {
     public ToolboxRenderer(BlockEntityRendererFactory.Context context) {
-        super(context);
     }
 
     @Override
-    protected void renderSafe(
-        ToolboxBlockEntity blockEntity,
-        float partialTicks,
-        MatrixStack ms,
-        VertexConsumerProvider buffer,
-        int light,
-        int overlay
-    ) {
-
-        BlockState blockState = blockEntity.getCachedState();
-        Direction facing = blockState.get(ToolboxBlock.FACING).getOpposite();
-        SuperByteBuffer lid = CachedBuffers.partial(AllPartialModels.TOOLBOX_LIDS.get(blockEntity.getColor()), blockState);
-        SuperByteBuffer drawer = CachedBuffers.partial(AllPartialModels.TOOLBOX_DRAWER, blockState);
-
-        float lidAngle = blockEntity.lid.getValue(partialTicks);
-        float drawerOffset = blockEntity.drawers.getValue(partialTicks);
-
-        VertexConsumer builder = buffer.getBuffer(RenderLayer.getCutoutMipped());
-        lid.center().rotateYDegrees(-facing.getPositiveHorizontalDegrees()).uncenter().translate(0, 6 / 16f, 12 / 16f).rotateXDegrees(135 * lidAngle)
-            .translate(0, -6 / 16f, -12 / 16f).light(light).renderInto(ms, builder);
-
-        for (int offset : Iterate.zeroAndOne) {
-            drawer.center().rotateYDegrees(-facing.getPositiveHorizontalDegrees()).uncenter()
-                .translate(0, offset * 1 / 8f, -drawerOffset * .175f * (2 - offset)).light(light).renderInto(ms, builder);
-        }
-
+    public ToolboxRenderState createRenderState() {
+        return new ToolboxRenderState();
     }
 
+    @Override
+    public void updateRenderState(
+        ToolboxBlockEntity be,
+        ToolboxRenderState state,
+        float tickProgress,
+        Vec3d cameraPos,
+        @Nullable ModelCommandRenderer.CrumblingOverlayCommand crumblingOverlay
+    ) {
+        BlockEntityRenderState.updateBlockEntityRenderState(be, state, crumblingOverlay);
+        state.layer = RenderLayer.getCutoutMipped();
+        state.lid = CachedBuffers.partial(AllPartialModels.TOOLBOX_LIDS.get(be.getColor()), state.blockState);
+        state.drawer = CachedBuffers.partial(AllPartialModels.TOOLBOX_DRAWER, state.blockState);
+        Direction facing = state.blockState.get(ToolboxBlock.FACING).getOpposite();
+        state.yRot = MathHelper.RADIANS_PER_DEGREE * -facing.getPositiveHorizontalDegrees();
+        state.xRot = MathHelper.RADIANS_PER_DEGREE * 135 * be.lid.getValue(tickProgress);
+        float drawerOffset = be.drawers.getValue(tickProgress);
+        state.offset1 = -drawerOffset * .175f;
+        state.offset2 = state.offset1 * 2;
+    }
+
+    @Override
+    public void render(ToolboxRenderState state, MatrixStack matrices, OrderedRenderCommandQueue queue, CameraRenderState cameraState) {
+        queue.submitCustom(matrices, state.layer, state);
+    }
+
+    public static class ToolboxRenderState extends BlockEntityRenderState implements OrderedRenderCommandQueue.Custom {
+        public RenderLayer layer;
+        public SuperByteBuffer lid;
+        public SuperByteBuffer drawer;
+        public float yRot;
+        public float xRot;
+        public float offset1;
+        public float offset2;
+
+        @Override
+        public void render(MatrixStack.Entry matricesEntry, VertexConsumer vertexConsumer) {
+            lid.center().rotateY(yRot).uncenter().translate(0, 0.375f, 0.75f).rotateX(xRot).translate(0, -0.375f, -0.75f).light(lightmapCoordinates)
+                .renderInto(matricesEntry, vertexConsumer);
+            drawer.center().rotateY(yRot).uncenter().translate(0, 0.125f, offset1).light(lightmapCoordinates)
+                .renderInto(matricesEntry, vertexConsumer);
+            drawer.center().rotateY(yRot).uncenter().translate(0, 0, offset2).light(lightmapCoordinates).renderInto(matricesEntry, vertexConsumer);
+        }
+    }
 }
