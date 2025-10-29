26acd977268e69fa32d43c35e0e10463e178bc7c
ZurrTum <zurrtum@foxmail.com>
2025-10-29 10:01:24 +0800

    Ignore virtual world markDirty. Closes #46

--- src/client/java/com/zurrtum/create/client/foundation/virtualWorld/VirtualRenderWorld.java
+++ src/client/java/com/zurrtum/create/client/foundation/virtualWorld/VirtualRenderWorld.java
@@ -548,4 +548,8 @@ public class VirtualRenderWorld extends World implements VisualizationLevel {
     public int sectionIndexToCoord(int index) {
         return index + this.getBottomSectionCoord();
     }
+
+    @Override
+    public void markDirty(BlockPos pos) {
+    }
 }
