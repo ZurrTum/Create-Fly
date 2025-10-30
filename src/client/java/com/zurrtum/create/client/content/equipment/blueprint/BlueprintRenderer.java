6a51ba60380742eb3904b5d1b435fa255634974d
ZurrTum <zurrtum@foxmail.com>
2025-10-30 23:48:40 +0800

    BlueprintRenderer

--- src/client/java/com/zurrtum/create/client/content/equipment/blueprint/BlueprintRenderer.java
+++ src/client/java/com/zurrtum/create/client/content/equipment/blueprint/BlueprintRenderer.java
@@ -5,114 +5,35 @@ import com.zurrtum.create.client.AllPartialModels;
 import com.zurrtum.create.client.catnip.render.CachedBuffers;
 import com.zurrtum.create.client.catnip.render.SuperByteBuffer;
 import com.zurrtum.create.client.flywheel.lib.model.baked.PartialModel;
-import com.zurrtum.create.client.flywheel.lib.transform.TransformStack;
 import com.zurrtum.create.content.equipment.blueprint.BlueprintEntity;
 import net.minecraft.block.Blocks;
-import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.item.ItemModelManager;
 import net.minecraft.client.render.OverlayTexture;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.TexturedRenderLayers;
-import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.command.OrderedRenderCommandQueue;
 import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.state.EntityRenderState;
-import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.render.item.ItemRenderState;
+import net.minecraft.client.render.state.CameraRenderState;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemDisplayContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.RotationAxis;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.joml.Matrix3f;
+import org.joml.Matrix4f;
 
 public class BlueprintRenderer extends EntityRenderer<BlueprintEntity, BlueprintRenderer.BlueprintState> {
+    protected final ItemModelManager itemModelManager;
+
     public BlueprintRenderer(EntityRendererFactory.Context context) {
         super(context);
-    }
-
-    @Override
-    @SuppressWarnings("unchecked")
-    public void updateRenderState(BlueprintEntity entity, BlueprintState state, float tickProgress) {
-        state.yaw = entity.getLerpedYaw(tickProgress);
-        state.world = entity.getEntityWorld();
-        state.pitch = entity.getPitch();
-        int size = entity.size;
-        Couple<ItemStack>[] sections = new Couple[size * size];
-        for (int x = 0; x < size; x++) {
-            for (int y = 0; y < size; y++) {
-                int index = x * size + y;
-                sections[index] = entity.getSection(index).getDisplayItems();
-            }
-        }
-        state.size = size;
-        state.sections = sections;
-    }
-
-    @Override
-    public void render(BlueprintState state, MatrixStack ms, VertexConsumerProvider buffer, int light) {
-        PartialModel partialModel = state.size == 3 ? AllPartialModels.CRAFTING_BLUEPRINT_3x3 : state.size == 2 ? AllPartialModels.CRAFTING_BLUEPRINT_2x2 : AllPartialModels.CRAFTING_BLUEPRINT_1x1;
-        SuperByteBuffer sbb = CachedBuffers.partial(partialModel, Blocks.AIR.getDefaultState());
-        sbb.rotateYDegrees(-state.yaw).rotateXDegrees(90.0F + state.pitch).translate(-.5, -1 / 32f, -.5);
-        if (state.size == 2)
-            sbb.translate(.5, 0, -.5);
-
-        sbb.disableDiffuse().light(light).renderInto(ms, buffer.getBuffer(TexturedRenderLayers.getEntitySolid()));
-
-        ms.push();
-
-        float fakeNormalXRotation = -15;
-        int bl = light >> 4 & 0xf;
-        int sl = light >> 20 & 0xf;
-        boolean vertical = state.pitch != 0;
-        if (state.pitch == -90)
-            fakeNormalXRotation = -45;
-        else if (state.pitch == 90 || state.yaw % 180 != 0) {
-            bl /= 1.35;
-            sl /= 1.35;
-        }
-        int itemLight = MathHelper.floor(sl + .5) << 20 | (MathHelper.floor(bl + .5) & 0xf) << 4;
-
-        TransformStack.of(ms).rotateYDegrees(vertical ? 0 : -state.yaw).rotateXDegrees(fakeNormalXRotation);
-        Matrix3f copy = new Matrix3f(ms.peek().getNormalMatrix());
-
-        ms.pop();
-        ms.push();
-
-        TransformStack.of(ms).rotateYDegrees(-state.yaw).rotateXDegrees(state.pitch).translate(0, 0, 1 / 32f + .001);
-
-        if (state.size == 3)
-            ms.translate(-1, -1, 0);
-
-        MatrixStack squashedMS = new MatrixStack();
-        squashedMS.peek().getPositionMatrix().mul(ms.peek().getPositionMatrix());
-
-        ItemRenderer itemRenderer = MinecraftClient.getInstance().getItemRenderer();
-        for (int x = 0; x < state.size; x++) {
-            squashedMS.push();
-            for (int y = 0; y < state.size; y++) {
-                squashedMS.push();
-                squashedMS.scale(.5f, .5f, 1 / 1024f);
-                state.sections[x * state.size + y].forEachWithContext((stack, primary) -> {
-                    if (stack.isEmpty())
-                        return;
-
-                    squashedMS.push();
-                    if (!primary) {
-                        squashedMS.translate(0.325f, -0.325f, 1);
-                        squashedMS.scale(.625f, .625f, 1);
-                    }
-
-                    squashedMS.peek().getNormalMatrix().set(copy);
-
-                    itemRenderer.renderItem(stack, ItemDisplayContext.GUI, itemLight, OverlayTexture.DEFAULT_UV, squashedMS, buffer, state.world, 0);
-                    squashedMS.pop();
-                });
-                squashedMS.pop();
-                squashedMS.translate(1, 0, 0);
-            }
-            squashedMS.pop();
-            squashedMS.translate(0, 1, 0);
-        }
-
-        ms.pop();
+        itemModelManager = context.getItemModelManager();
     }
 
     @Override
@@ -120,11 +41,136 @@ public class BlueprintRenderer extends EntityRenderer<BlueprintEntity, Blueprint
         return new BlueprintState();
     }
 
-    public static class BlueprintState extends EntityRenderState {
+    @Override
+    public void updateRenderState(BlueprintEntity entity, BlueprintState state, float tickProgress) {
+        super.updateRenderState(entity, state, tickProgress);
+        float yaw = entity.getLerpedYaw(tickProgress);
+        float pitch = entity.getPitch();
+        int size = entity.size;
+        state.layer = TexturedRenderLayers.getEntitySolid();
+        PartialModel partialModel = size == 3 ? AllPartialModels.CRAFTING_BLUEPRINT_3x3 : size == 2 ? AllPartialModels.CRAFTING_BLUEPRINT_2x2 : AllPartialModels.CRAFTING_BLUEPRINT_1x1;
+        state.model = CachedBuffers.partial(partialModel, Blocks.AIR.getDefaultState());
+        state.yRot = MathHelper.RADIANS_PER_DEGREE * -yaw;
+        state.xRot = MathHelper.RADIANS_PER_DEGREE * (90.0F + pitch);
+        Vec3d offset = new Vec3d(-.5, -1 / 32f, -.5);
+        if (size == 2) {
+            offset = offset.add(.5, 0, -.5);
+        }
+        state.offset = offset;
+        World world = entity.getEntityWorld();
+        int itemSize = size * size * 2;
+        ItemRenderState[] items = new ItemRenderState[itemSize];
+        boolean empty = true;
+        for (int i = 0; i < itemSize; ) {
+            Couple<ItemStack> displayItems = entity.getSection(i >> 1).getDisplayItems();
+            ItemStack firstStack = displayItems.getFirst();
+            if (!firstStack.isEmpty()) {
+                empty = false;
+                items[i] = createItemRenderState(itemModelManager, firstStack, world);
+            }
+            i++;
+            ItemStack secondStack = displayItems.getSecond();
+            if (!secondStack.isEmpty()) {
+                empty = false;
+                items[i] = createItemRenderState(itemModelManager, secondStack, world);
+            }
+            i++;
+        }
+        if (empty) {
+            return;
+        }
+        state.items = items;
+        state.size = size;
+        int bl = state.light >> 4 & 0xf;
+        int sl = state.light >> 20 & 0xf;
+        state.normalYRot = pitch != 0 ? 0 : state.yRot;
+        if (pitch == -90) {
+            state.normalXRot = MathHelper.RADIANS_PER_DEGREE * -45;
+        } else if (pitch == 90 || yaw % 180 != 0) {
+            state.normalXRot = MathHelper.RADIANS_PER_DEGREE * -15;
+            bl = (int) (bl / 1.35);
+            sl = (int) (sl / 1.35);
+        } else {
+            state.normalXRot = MathHelper.RADIANS_PER_DEGREE * -15;
+        }
+        state.itemXRot = MathHelper.RADIANS_PER_DEGREE * pitch;
+        state.itemOffsetZ = 1 / 32f + .001f;
+        if (size == 3) {
+            state.itemOffsetXY = -1;
+        }
+        state.itemLight = MathHelper.floor(sl + .5) << 20 | (MathHelper.floor(bl + .5) & 0xf) << 4;
+    }
+
+    private static ItemRenderState createItemRenderState(ItemModelManager itemModelManager, ItemStack stack, World world) {
+        ItemRenderState state = new ItemRenderState();
+        state.displayContext = ItemDisplayContext.GUI;
+        itemModelManager.update(state, stack, state.displayContext, world, null, 0);
+        return state;
+    }
+
+    public void render(BlueprintState state, MatrixStack matrices, OrderedRenderCommandQueue queue, CameraRenderState cameraState) {
+        queue.submitCustom(matrices, state.layer, state);
+        ItemRenderState[] items = state.items;
+        if (items == null) {
+            return;
+        }
+        matrices.push();
+        MatrixStack.Entry entry = matrices.peek();
+        Matrix3f normal = entry.getNormalMatrix();
+        if (state.normalYRot != 0) {
+            normal.rotate(RotationAxis.POSITIVE_Y.rotation(state.normalYRot));
+        }
+        normal.rotate(RotationAxis.POSITIVE_X.rotation(state.normalXRot));
+        Matrix4f pose = entry.getPositionMatrix();
+        pose.rotate(RotationAxis.POSITIVE_Y.rotation(state.yRot));
+        pose.rotate(RotationAxis.POSITIVE_X.rotation(state.itemXRot));
+        pose.translate(state.itemOffsetXY, state.itemOffsetXY, state.itemOffsetZ);
+        Matrix4f copy = new Matrix4f(pose);
+        int light = state.itemLight;
+        for (int i = 0, size = items.length, n = 0, w = state.size - 1; i < size; ) {
+            ItemRenderState firstState = items[i++];
+            ItemRenderState secondState = items[i++];
+            if (firstState != null || secondState != null) {
+                pose.scale(0.5f, 0.5f, 0.0009765625f);
+                if (firstState != null) {
+                    firstState.render(matrices, queue, light, OverlayTexture.DEFAULT_UV, 0);
+                }
+                if (secondState != null) {
+                    pose.translate(0.325f, -0.325f, 1);
+                    pose.scale(.625f, .625f, 1);
+                    secondState.render(matrices, queue, light, OverlayTexture.DEFAULT_UV, 0);
+                }
+            }
+            if (n < w) {
+                copy.translate(1, 0, 0);
+                n++;
+            } else {
+                copy.translate(-w, 1, 0);
+                n = 0;
+            }
+            pose.set(copy);
+        }
+        matrices.pop();
+    }
+
+    public static class BlueprintState extends EntityRenderState implements OrderedRenderCommandQueue.Custom {
+        public RenderLayer layer;
+        public SuperByteBuffer model;
+        public float yRot;
+        public float xRot;
+        public Vec3d offset;
+        public ItemRenderState[] items;
         public int size;
-        public float yaw;
-        public World world;
-        public float pitch;
-        public Couple<ItemStack>[] sections;
+        public float normalYRot;
+        public float normalXRot;
+        public float itemXRot;
+        public int itemOffsetXY;
+        public float itemOffsetZ;
+        public int itemLight;
+
+        @Override
+        public void render(MatrixStack.Entry matricesEntry, VertexConsumer vertexConsumer) {
+            model.rotateY(yRot).rotateX(xRot).translate(offset).disableDiffuse().light(light).renderInto(matricesEntry, vertexConsumer);
+        }
     }
 }
