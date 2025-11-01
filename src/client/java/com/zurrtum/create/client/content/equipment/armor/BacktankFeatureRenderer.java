6b0117b0dd02235ed8754d121659a64ac12b87ef
ZurrTum <zurrtum@foxmail.com>
2025-11-02 04:38:30 +0800

    BacktankFeatureRenderer

--- src/client/java/com/zurrtum/create/client/content/equipment/armor/BacktankFeatureRenderer.java
+++ src/client/java/com/zurrtum/create/client/content/equipment/armor/BacktankFeatureRenderer.java
@@ -9,7 +9,7 @@ import com.zurrtum.create.content.equipment.armor.BacktankItem;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.render.TexturedRenderLayers;
 import net.minecraft.client.render.VertexConsumer;
-import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.command.OrderedRenderCommandQueue;
 import net.minecraft.client.render.entity.feature.FeatureRenderer;
 import net.minecraft.client.render.entity.feature.FeatureRendererContext;
 import net.minecraft.client.render.entity.model.BipedEntityModel;
@@ -17,6 +17,7 @@ import net.minecraft.client.render.entity.state.BipedEntityRenderState;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.EntityPose;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.MathHelper;
 
 public class BacktankFeatureRenderer<S extends BipedEntityRenderState, M extends BipedEntityModel<? super S>> extends FeatureRenderer<S, M> {
     public BacktankFeatureRenderer(FeatureRendererContext<S, M> context) {
@@ -24,32 +25,41 @@ public class BacktankFeatureRenderer<S extends BipedEntityRenderState, M extends
     }
 
     @Override
-    public void render(MatrixStack ms, VertexConsumerProvider buffer, int light, S state, float limbAngle, float limbDistance) {
-        if (state.pose == EntityPose.SLEEPING || !(state.equippedChestStack.getItem() instanceof BacktankItem item)) {
+    public void render(MatrixStack ms, OrderedRenderCommandQueue queue, int light, S entityState, float limbAngle, float limbDistance) {
+        if (entityState.pose == EntityPose.SLEEPING || !(entityState.equippedChestStack.getItem() instanceof BacktankItem item)) {
             return;
         }
-
-        VertexConsumer vc = buffer.getBuffer(TexturedRenderLayers.getEntityCutout());
-        BlockState renderedState = item.getBlock().getDefaultState().with(BacktankBlock.HORIZONTAL_FACING, Direction.SOUTH);
-        SuperByteBuffer backtank = CachedBuffers.block(renderedState);
-        SuperByteBuffer cogs = CachedBuffers.partial(BacktankRenderer.getCogsModel(renderedState), renderedState);
-        SuperByteBuffer nob = CachedBuffers.partial(BacktankRenderer.getShaftModel(renderedState), renderedState);
+        BacktankRenderState state = new BacktankRenderState();
+        BlockState blockState = item.getBlock().getDefaultState().with(BacktankBlock.HORIZONTAL_FACING, Direction.SOUTH);
+        state.backtank = CachedBuffers.block(blockState);
+        state.cogs = CachedBuffers.partial(BacktankRenderer.getCogsModel(blockState), blockState);
+        state.nob = CachedBuffers.partial(BacktankRenderer.getShaftModel(blockState), blockState);
+        state.light = light;
+        state.yRot = MathHelper.RADIANS_PER_DEGREE * 180;
+        state.angle = AngleHelper.rad(2 * AnimationTickHolder.getRenderTime() % 360);
 
         ms.push();
-
         getContextModel().body.applyTransform(ms);
         ms.translate(-1 / 2f, 10 / 16f, 1f);
         ms.scale(1, -1, -1);
-
-        backtank.disableDiffuse().light(light).renderInto(ms, vc);
-
-        nob.disableDiffuse().translate(0, -3f / 16, 0).light(light).renderInto(ms, vc);
-
-        cogs.center().rotateYDegrees(180).uncenter().translate(0, 6.5f / 16, 11f / 16)
-            .rotate(AngleHelper.rad(2 * AnimationTickHolder.getRenderTime() % 360), Direction.EAST).translate(0, -6.5f / 16, -11f / 16);
-
-        cogs.disableDiffuse().light(light).renderInto(ms, vc);
-
+        queue.submitCustom(ms, TexturedRenderLayers.getEntityCutout(), state);
         ms.pop();
+    }
+
+    public static class BacktankRenderState implements OrderedRenderCommandQueue.Custom {
+        public SuperByteBuffer backtank;
+        public SuperByteBuffer cogs;
+        public SuperByteBuffer nob;
+        public int light;
+        public float yRot;
+        public float angle;
+
+        @Override
+        public void render(MatrixStack.Entry matricesEntry, VertexConsumer vertexConsumer) {
+            backtank.disableDiffuse().light(light).renderInto(matricesEntry, vertexConsumer);
+            nob.disableDiffuse().translate(0, -0.1875f, 0).light(light).renderInto(matricesEntry, vertexConsumer);
+            cogs.center().rotateY(yRot).uncenter().translate(0, 0.40625f, 0.6875f).rotate(angle, Direction.EAST).translate(0, -0.40625f, -0.6875f);
+            cogs.disableDiffuse().light(light).renderInto(matricesEntry, vertexConsumer);
+        }
     }
 }
