3cb5024ee4f029065079bc15cf8c33e04195ec5c
ZurrTum <zurrtum@foxmail.com>
2025-11-19 03:49:37 +0100

    Add filter null check. Closes #74

--- src/client/java/com/zurrtum/create/client/foundation/blockEntity/behaviour/filtering/SidedFilteringBehaviour.java
+++ src/client/java/com/zurrtum/create/client/foundation/blockEntity/behaviour/filtering/SidedFilteringBehaviour.java
@@ -41,6 +41,9 @@ public class SidedFilteringBehaviour extends FilteringBehaviour<ServerSidedFilte
         if (sidedFilter != null && sidedFilter.behaviour != null) {
             return sidedFilter;
         }
+        if (behaviour == null) {
+            return null;
+        }
         ServerFilteringBehaviour filter = behaviour.get(side);
         if (filter == null) {
             return null;
