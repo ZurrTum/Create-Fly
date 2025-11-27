44a4f72f2b41aecf1d75d461c26cd8988c80bbcf
ZurrTum <zurrtum@foxmail.com>
2025-11-27 17:32:55 +0800

    Fix Haunted Bell effect in multi-threaded. Closes #80

--- src/client/java/com/zurrtum/create/client/content/equipment/bell/SoulPulseEffectHandler.java
+++ src/client/java/com/zurrtum/create/client/content/equipment/bell/SoulPulseEffectHandler.java
@@ -1,59 +1,86 @@
 package com.zurrtum.create.client.content.equipment.bell;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
 
-public class SoulPulseEffectHandler {
+import java.util.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
+public class SoulPulseEffectHandler {
     private final List<SoulPulseEffect> pulses;
+    private final Queue<SoulPulseEffect> queue;
     private final Set<BlockPos> occupied;
 
     public SoulPulseEffectHandler() {
-        pulses = new ArrayList<>();
+        pulses = new LinkedList<>();
+        queue = new ConcurrentLinkedQueue<>();
         occupied = new HashSet<>();
     }
 
     public void tick(Level world) {
-        for (SoulPulseEffect pulse : pulses) {
+        Iterator<SoulPulseEffect> iterator = pulses.iterator();
+        SoulPulseEffect pulse;
+        while (iterator.hasNext()) {
+            pulse = iterator.next();
             List<BlockPos> spawns = pulse.tick(world);
-            if (spawns == null)
-                continue;
-
-            if (pulse.canOverlap()) {
-                for (BlockPos pos : spawns) {
-                    pulse.spawnParticles(world, pos);
+            if (spawns != null) {
+                if (pulse.canOverlap()) {
+                    for (BlockPos pos : spawns) {
+                        pulse.spawnParticles(world, pos);
+                    }
+                    if (pulse.finished()) {
+                        iterator.remove();
+                    }
+                } else if (pulse.finished()) {
+                    for (BlockPos pos : spawns) {
+                        if (occupied.contains(pos)) {
+                            continue;
+                        }
+                        pulse.spawnParticles(world, pos);
+                    }
+                    pulse.added.forEach(occupied::remove);
+                    iterator.remove();
+                } else {
+                    for (BlockPos pos : spawns) {
+                        if (occupied.add(pos)) {
+                            pulse.spawnParticles(world, pos);
+                            pulse.added.add(pos);
+                        }
+                    }
                 }
-            } else {
-                for (BlockPos pos : spawns) {
-                    if (occupied.contains(pos))
-                        continue;
-
-                    pulse.spawnParticles(world, pos);
-                    pulse.added.add(pos);
-                    occupied.add(pos);
+            } else if (pulse.finished()) {
+                if (!pulse.canOverlap()) {
+                    pulse.added.forEach(occupied::remove);
                 }
+                iterator.remove();
             }
         }
-
-        for (SoulPulseEffect pulse : pulses) {
-            if (pulse.finished() && !pulse.canOverlap())
-                occupied.removeAll(pulse.added);
+        while ((pulse = queue.poll()) != null) {
+            List<BlockPos> spawns = pulse.tick(world);
+            if (spawns != null) {
+                if (pulse.canOverlap()) {
+                    for (BlockPos pos : spawns) {
+                        pulse.spawnParticles(world, pos);
+                    }
+                } else {
+                    for (BlockPos pos : spawns) {
+                        if (occupied.add(pos)) {
+                            pulse.spawnParticles(world, pos);
+                            pulse.added.add(pos);
+                        }
+                    }
+                }
+            }
+            pulses.add(pulse);
         }
-        pulses.removeIf(SoulPulseEffect::finished);
     }
 
     public void addPulse(SoulPulseEffect pulse) {
-        pulses.add(pulse);
+        queue.offer(pulse);
     }
 
     public void refresh() {
         pulses.clear();
         occupied.clear();
     }
-
 }
