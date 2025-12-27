a71d8c5702f4c3c637ef2a112ed071ee63849866
ZurrTum <zurrtum@foxmail.com>
2025-12-27 16:50:53 +0800

    Fix computercraft register

--- src/main/java/com/zurrtum/create/compat/CompatMod.java
+++ src/main/java/com/zurrtum/create/compat/CompatMod.java
@@ -1,11 +1,17 @@
 package com.zurrtum.create.compat;
 
+import com.zurrtum.create.compat.computercraft.AllComputerDisplaySource;
+import com.zurrtum.create.compat.computercraft.ComputerCraftProxy;
 import com.zurrtum.create.compat.trinkets.GoggleTrinket;
 
 public class CompatMod {
     public static void register() {
         if (Mods.TRINKETS.isLoaded()) {
             GoggleTrinket.register();
+        }
+        if (Mods.COMPUTERCRAFT.isLoaded()) {
+            ComputerCraftProxy.register();
+            AllComputerDisplaySource.register();
         }
     }
 }
