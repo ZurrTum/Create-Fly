65950a3b326938fd8c29e1b5303691edb92f6e27
ZurrTum <zurrtum@foxmail.com>
2026-01-14 20:48:34 +0800

    Update Lunar Client compat

--- src/main/java/com/zurrtum/create/mixin/FluidsMixin.java
+++ src/main/java/com/zurrtum/create/mixin/FluidsMixin.java
@@ -2,6 +2,7 @@ package com.zurrtum.create.mixin;
 
 import com.zurrtum.create.api.registry.CreateRegisterPlugin;
 import net.minecraft.fluid.Fluids;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -9,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Fluids.class)
 public class FluidsMixin {
-    @Inject(method = "<clinit>", at = @At(value = "INVOKE", target = "net/minecraft/registry/DefaultedRegistry.iterator()Ljava/util/Iterator;"))
+    @Inject(method = "<clinit>", at = @At(value = "FIELD", target = "Lnet/minecraft/registry/Registries;FLUID:Lnet/minecraft/registry/DefaultedRegistry;", opcode = Opcodes.GETSTATIC))
     private static void register(CallbackInfo ci) {
         CreateRegisterPlugin.registerFluid();
     }
