f2a0a9b0686ecc64afdfa330af0f5d26adb073fd
ZurrTum <zurrtum@foxmail.com>
2026-01-19 19:06:42 +0800

    Update mixin

--- src/client/java/com/zurrtum/create/client/mixin/RenderSystemMixin.java
+++ src/client/java/com/zurrtum/create/client/mixin/RenderSystemMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(RenderSystem.class)
 public class RenderSystemMixin {
-    @Inject(method = "initRenderer", at = @At("RETURN"))
+    @Inject(method = "initRenderer(Lcom/mojang/blaze3d/systems/GpuDevice;)V", at = @At("RETURN"), remap = false)
     private static void flywheel$onInitRenderer(CallbackInfo ci) {
         GlCompat.init();
     }
--- src/main/java/com/zurrtum/create/mixin/ChunkMapMixin.java
+++ src/main/java/com/zurrtum/create/mixin/ChunkMapMixin.java
@@ -1,21 +1,18 @@
 package com.zurrtum.create.mixin;
 
-import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.zurrtum.create.content.contraptions.minecart.capability.CapabilityMinecartController;
-import net.minecraft.server.level.ChunkHolder;
 import net.minecraft.server.level.ChunkMap;
 import net.minecraft.world.level.chunk.LevelChunk;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.concurrent.CompletableFuture;
 
 @Mixin(ChunkMap.class)
 public class ChunkMapMixin {
-    @Inject(method = "lambda$scheduleUnload$0(Lnet/minecraft/server/level/ChunkHolder;Ljava/util/concurrent/CompletableFuture;J)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/chunk/LevelChunk;setLoaded(Z)V", shift = At.Shift.AFTER))
-    private void tryUnloadChunk(ChunkHolder chunkHolder, CompletableFuture<?> completableFuture, long l, CallbackInfo ci, @Local LevelChunk chunk) {
+    @WrapOperation(method = "lambda$scheduleUnload$0(Lnet/minecraft/server/level/ChunkHolder;Ljava/util/concurrent/CompletableFuture;J)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/chunk/LevelChunk;setLoaded(Z)V"))
+    private void tryUnloadChunk(LevelChunk chunk, boolean loaded, Operation<Void> original) {
+        original.call(chunk, loaded);
         CapabilityMinecartController.onChunkUnloaded(chunk);
     }
 }
