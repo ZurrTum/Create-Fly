7a7d32e148070b34d004648c8c7feb93d0dc5a52
ZurrTum <zurrtum@foxmail.com>
2026-01-26 12:09:12 +0800

    Fix Cardboard compat. Closes #170

--- src/main/java/com/zurrtum/create/mixin/BlockItemMixin.java
+++ src/main/java/com/zurrtum/create/mixin/BlockItemMixin.java
@@ -5,9 +5,15 @@ import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.Share;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
+import com.zurrtum.create.content.contraptions.glue.SuperGlueHandler;
+import com.zurrtum.create.content.equipment.symmetryWand.SymmetryHandler;
 import com.zurrtum.create.content.equipment.symmetryWand.SymmetryPlacementContext;
 import com.zurrtum.create.foundation.item.ItemPlacementSoundContext;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.item.context.UseOnContext;
@@ -15,15 +21,46 @@ import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BlockItem.class)
 public class BlockItemMixin {
     @WrapOperation(method = "useOn(Lnet/minecraft/world/item/context/UseOnContext;)Lnet/minecraft/world/InteractionResult;", at = @At(value = "NEW", target = "(Lnet/minecraft/world/item/context/UseOnContext;)Lnet/minecraft/world/item/context/BlockPlaceContext;"))
-    private BlockPlaceContext replaceContext(UseOnContext context, Operation<BlockPlaceContext> original) {
-        if (context instanceof SymmetryPlacementContext placementContext) {
-            return placementContext;
+    private BlockPlaceContext replaceContext(
+        UseOnContext context,
+        Operation<BlockPlaceContext> original,
+        @Share("place") LocalRef<BlockPlaceContext> place
+    ) {
+        if (context.getLevel().isClientSide()) {
+            return original.call(context);
         }
-        return original.call(context);
+        if (context instanceof SymmetryPlacementContext placementContext) {
+            place.set(placementContext);
+            return placementContext;
+        } else {
+            BlockPlaceContext blockContext = original.call(context);
+            place.set(blockContext);
+            return blockContext;
+        }
+    }
+
+    @Inject(method = "useOn(Lnet/minecraft/world/item/context/UseOnContext;)Lnet/minecraft/world/InteractionResult;", at = @At("RETURN"))
+    private void useOn(UseOnContext useOnContext, CallbackInfoReturnable<InteractionResult> cir, @Share("place") LocalRef<BlockPlaceContext> place) {
+        if (cir.getReturnValue().consumesAction()) {
+            BlockPlaceContext context = place.get();
+            if (context != null) {
+                Player player = context.getPlayer();
+                if (player != null) {
+                    ServerLevel world = (ServerLevel) context.getLevel();
+                    BlockPos pos = context.getClickedPos();
+                    SuperGlueHandler.glueListensForBlockPlacement(world, player, pos);
+                    if (!(context instanceof SymmetryPlacementContext)) {
+                        SymmetryHandler.onBlockPlaced(world, player, pos, context);
+                    }
+                }
+            }
+        }
     }
 
     @WrapOperation(method = "place(Lnet/minecraft/world/item/context/BlockPlaceContext;)Lnet/minecraft/world/InteractionResult;", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;getSoundType()Lnet/minecraft/world/level/block/SoundType;"))
