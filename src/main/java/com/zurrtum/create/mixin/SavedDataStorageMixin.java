cec68e8c2cc45a1230cd147bb17e461b71242636
ZurrTum <zurrtum@foxmail.com>
2026-02-04 05:54:01 +0800

    DimensionDataStorage -> SavedDataStorage

--- src/main/java/com/zurrtum/create/mixin/SavedDataStorageMixin.java
+++ src/main/java/com/zurrtum/create/mixin/SavedDataStorageMixin.java
@@ -5,13 +5,13 @@ import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.mojang.datafixers.DataFixer;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.util.datafix.DataFixTypes;
-import net.minecraft.world.level.storage.DimensionDataStorage;
+import net.minecraft.world.level.storage.SavedDataStorage;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Mixin(DimensionDataStorage.class)
-public class DimensionDataStorageMixin {
-    @WrapOperation(method = "readTagFromDisk(Ljava/lang/String;Lnet/minecraft/util/datafix/DataFixTypes;I)Lnet/minecraft/nbt/CompoundTag;", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/datafix/DataFixTypes;update(Lcom/mojang/datafixers/DataFixer;Lnet/minecraft/nbt/CompoundTag;II)Lnet/minecraft/nbt/CompoundTag;"))
+@Mixin(SavedDataStorage.class)
+public class SavedDataStorageMixin {
+    @WrapOperation(method = "readTagFromDisk(Ljava/nio/file/Path;Lnet/minecraft/util/datafix/DataFixTypes;I)Lnet/minecraft/nbt/CompoundTag;", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/datafix/DataFixTypes;update(Lcom/mojang/datafixers/DataFixer;Lnet/minecraft/nbt/CompoundTag;II)Lnet/minecraft/nbt/CompoundTag;"))
     private CompoundTag handleNullDataFixType(
         DataFixTypes dataFixTypes,
         DataFixer dataFixer,
