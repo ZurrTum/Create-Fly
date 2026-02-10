95a55cfb6c70449a3836c072c9b41d4d19b5a5b5
ZurrTum <zurrtum@foxmail.com>
2026-02-11 01:16:18 +0800

    Fix JEI tooltip position

--- src/client/java/com/zurrtum/create/client/compat/jei/widget/ChanceTooltip.java
+++ src/client/java/com/zurrtum/create/client/compat/jei/widget/ChanceTooltip.java
@@ -1,19 +1,21 @@
 package com.zurrtum.create.client.compat.jei.widget;
 
+import com.mojang.datafixers.util.Either;
 import com.zurrtum.create.client.foundation.utility.CreateLang;
 import mezz.jei.api.gui.builder.ITooltipBuilder;
 import mezz.jei.api.gui.ingredient.IRecipeSlotRichTooltipCallback;
 import mezz.jei.api.gui.ingredient.IRecipeSlotView;
 import net.minecraft.ChatFormatting;
-import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.FormattedText;
+import net.minecraft.world.inventory.tooltip.TooltipComponent;
 
-public record ChanceTooltip(Component chance) implements IRecipeSlotRichTooltipCallback {
+public record ChanceTooltip(Either<FormattedText, TooltipComponent> chance) implements IRecipeSlotRichTooltipCallback {
     public ChanceTooltip(float chance) {
-        this(CreateLang.translateDirect("recipe.processing.chance", chance < 0.01 ? "<1" : (int) (chance * 100)).withStyle(ChatFormatting.GOLD));
+        this(Either.left(CreateLang.translateDirect("recipe.processing.chance", chance < 0.01 ? "<1" : (int) (chance * 100)).withStyle(ChatFormatting.GOLD)));
     }
 
     @Override
     public void onRichTooltip(IRecipeSlotView recipeSlotView, ITooltipBuilder tooltip) {
-        tooltip.add(chance);
+        tooltip.getLines().add(1, chance);
     }
 }
